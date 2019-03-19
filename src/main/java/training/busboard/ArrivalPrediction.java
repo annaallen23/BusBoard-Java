cc4e3359ac0d3c911219157245db6133db0aa993
Anna Allen <Anna.kettlewell@yahoo.co.uk>
2019-03-19 12:13:49 +0000

    console takes Bus Stop Input

---
+++ src/main/java/training/busboard/ArrivalPrediction.java
@@ -0,0 +1,28 @@
+package training.busboard;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+//tells jersey to ignore anything that isn't field below
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class ArrivalPrediction {
+
+    private String lineName;
+    private String destinationName;
+    private int timeToStation;
+    // Jersey doesn't need parameters it works like magic!
+    private ArrivalPrediction (){}
+
+    public String getLineName() {
+        return lineName;
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public int getTimeToStation() {
+        return timeToStation;
+    }
+}
+
--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,19 +1,41 @@
 package training.busboard;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.glassfish.jersey.jackson.JacksonFeature;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String args[]) {
         Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
 
-        String name = client.target("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals")
-                .request(MediaType.TEXT_PLAIN)
-                .get(String.class);
-        System.out.println(name);
+        System.out.println("Please enter bus stop code:");
+
+        Scanner busStopCodeScanner = new Scanner(System.in);
+        String busStopCodeInput = busStopCodeScanner.next();
+
+        //Jersey won't read string change to List
+        List <ArrivalPrediction> response = client.target("https://api.tfl.gov.uk/StopPoint")
+                //adds on scanner input in URL
+                .path(busStopCodeInput)
+                .path("Arrivals")
+                //Application_JSON_type tells Jersey you're reading JSON
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                // Generictype tells jersey to accept list
+                .get(new GenericType<List<ArrivalPrediction>>(){});
+
+        // remember Comparator.comparing compares getTimeToStation within list responses and sorts ascending
+        response.sort(Comparator.comparing(ArrivalPrediction::getTimeToStation));
+        // prints our first 5 responses
+        System.out.println(response.subList(0,5));
     }
 }	
