3d382eca1a8768b53837c15e52d85e74187dd878
Anna Allen <Anna.kettlewell@yahoo.co.uk>
2019-03-19 17:06:46 +0000

    website start

--- src/main/java/training/busboard/web/BusInfo.java
+++ src/main/java/training/busboard/web/BusInfo.java
@@ -1,13 +1,30 @@
 package training.busboard.web;
 
+import training.busboard.ArrivalPrediction;
+import training.busboard.BusStopsWithin;
+
+import java.util.List;
+
 public class BusInfo {
     private final String postcode;
+    private final List<List<ArrivalPrediction>> arrivals;
+    private final List<BusStopsWithin> busStops;
 
-    public BusInfo(String postcode) {
+    public BusInfo(String postcode, List<List<ArrivalPrediction>> arrivals, List<BusStopsWithin> busStops) {
         this.postcode = postcode;
+        this.arrivals = arrivals;
+        this.busStops = busStops;
     }
 
     public String getPostcode() {
         return postcode;
+    }
+
+    public List<List<ArrivalPrediction>> getArrivals() {
+        return arrivals;
+    }
+
+    public List<BusStopsWithin> getBusStops() {
+        return busStops;
     }
 }
--- src/main/java/training/busboard/web/Website.java
+++ src/main/java/training/busboard/web/Website.java
@@ -1,11 +1,18 @@
 package training.busboard.web;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import training.busboard.*;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @EnableAutoConfiguration
@@ -18,8 +25,18 @@ public class Website {
 
     @RequestMapping("/busInfo")
     ModelAndView busInfo(@RequestParam("postcode") String postcode) {
-        return new ModelAndView("info", "busInfo", new BusInfo(postcode)) ;
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+        PostcodeLocator location = GetLocation.getLonAndLat(client, postcode);
+        List<BusStopsWithin> busStops = GetBusStops.locateStopsWithin1000Meters(client, location.getLatitude(), location.getLongitude());
+        List<List<ArrivalPrediction>> arrivals = new ArrayList<>();
+        for (BusStopsWithin busStop : busStops) {
+            List<ArrivalPrediction> arrivalsForThisStop = GetArrivalPredictions.getArrivals(client, busStop.getNaptanId());
+            arrivals.add(arrivalsForThisStop);
+        }
+
+        return new ModelAndView("info", "busInfo", new BusInfo(postcode, arrivals, busStops));
     }
+
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Website.class, args);
